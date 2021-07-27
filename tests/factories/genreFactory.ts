5a1341fa0ce80005243e1ac617725da6efd5792a
thiagoribb <thiribeiro142@gmail.com>
2021-07-27 15:45:16 -0300

    preparando review

--- tests/factories/genreFactory.ts
+++
@@ -1,6 +0,0 @@
-import connection from "../../src/database"
-import faker from "faker"
-
-export async function createGenre () {
-    await connection.query("INSERT INTO genres (name) VALUES ($1)", [faker.music.genre()])
-}
